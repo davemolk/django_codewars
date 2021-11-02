af68a73c8ac2fcf88e529e6927380c86238b9ea1
davemolk <davemolk@gmail.com>
2021-11-02 09:53:27 -0600

    update error message with user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,16 +1,36 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+User = get_user_model()
 
 
 class CustomUserCreationForm(UserCreationForm):
 
+    error_message = UserCreationForm.error_messages.update(
+        {
+            'duplicate_username': "This username has already been taken"
+        }
+    )
+
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ('email', 'username',)
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        
+        raise ValidationError(
+            self.error_messages["duplicate_username"]
+        )
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
-        model = get_user_model()
-        fields = ('email', 'username')
+        model = User
+        fields = ('email', 'username',)
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
 from .forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 class SignupPageView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
