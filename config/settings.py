a262f69b8570d568bba9633167a9ae642f6886f1
davemolk <davemolk@gmail.com>
2021-11-04 14:26:30 -0600

    local host

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['codewarscompanion.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
